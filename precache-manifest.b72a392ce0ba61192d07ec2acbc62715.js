70bd64e9e4b222825df8bf69340bcd9ab6258603
Ayodele Olalekan <longyarnz@gmail.com>
2020-05-16 21:19:59 +0100

    Updates

---
+++ precache-manifest.b72a392ce0ba61192d07ec2acbc62715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42aa54a82fb9b0275f7d0decaef53de",
+    "url": "/drag/index.html"
+  },
+  {
+    "revision": "18b5914f69d77bc42152",
+    "url": "/drag/static/css/main.2fd4d216.chunk.css"
+  },
+  {
+    "revision": "24ce4322fd9747314462",
+    "url": "/drag/static/js/2.5502aaaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drag/static/js/2.5502aaaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b5914f69d77bc42152",
+    "url": "/drag/static/js/main.4cf01218.chunk.js"
+  },
+  {
+    "revision": "e3471e21bcc851d6ab8b",
+    "url": "/drag/static/js/runtime-main.393a8a8a.js"
+  }
+]);
