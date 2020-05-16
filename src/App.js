40db4313015bf05c53e5012d5adc4790c61469b9
Ayodele Olalekan <longyarnz@gmail.com>
2020-05-16 14:39:49 +0100

    chore: Create add button for task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import AddButton from './components/AddButton';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="wrapper">
+        <AddButton />
+      </div>
     </div>
   );
 }
---
+++ src/components/AddButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddButton() {
+  return (
+    <button className="add-button">
+      <i className="material-icons">add</i>
+    </button>
+  )
+}
