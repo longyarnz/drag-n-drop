fdf128a9b21a45d3d45c926eb6b58320ef573093
Ayodele Olalekan <longyarnz@gmail.com>
2020-05-16 17:47:57 +0100

    chore: Create app store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './App.css';
-import List from './components/List';
+import React, { useReducer } from 'react';
 import { FlatList } from '@bit/lekanmedia.shared-ui.internal.utils';
+import List from './components/List';
+import { reducer, initialState } from './store'
+import './App.css';
 
 function App() {
-  const taskLists = ['Backlog', 'Todo', 'In Progress', 'In Review', 'Done'];
+  const store = useReducer(reducer, initialState);
+  const taskLists = Object.keys(store[0]);
 
   return (
     <div className="App">
       <FlatList
         list={taskLists}
         listView={(item, index) => (
-          <List key={index} item={item} />
+          <List key={index} item={item} store={store} />
         )}
       />
     </div>
---
+++ src/store.js
@@ -0,0 +1,14 @@
+export const initialState = {
+  Backlog: [],
+  Todo: [],
+  'In Progress': [],
+  'In Review': [],
+  Done: []
+}
+
+export function reducer(state, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
